ef81e5a621b001b0c2fee586de1a3be6d84d5b7d
FedericoDiGennaro <digennarof.00@gmail.com>
2022-10-18 19:40:46 +0200

    cross for ridge

--- helpers.py
+++ helpers.py
@@ -4,6 +4,28 @@
 import numpy as np
 import csv
 
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree.
+    
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        degree: integer.
+        
+    Returns:
+        poly: numpy array of shape (N,d+1)
+        
+    >>> build_poly(np.array([0.0, 1.5]), 2)
+    array([[1.  , 0.  , 0.  ],
+           [1.  , 1.5 , 2.25]])
+    """    
+    # ***************************************************
+    
+    phi=np.ones((len(x),1))
+    for i in range(1,degree+1):
+        phi=np.c_[phi,x**i]
+        
+    # ***************************************************
+    return phi
 
 def load_train_data(path,default_missing_value = -999):
     """Load data function. 
